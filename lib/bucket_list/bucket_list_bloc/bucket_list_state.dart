9243d2b804b29c7f49e2ba21e985269f753343f4
James Jilhaney <notail2111@gmail.com>
2023-09-14 17:03:18 +0800

    Refactor: BucketListState class

---
+++ lib/bucket_list/bucket_list_bloc/bucket_list_state.dart
@@ -0,0 +1,28 @@
+part of 'bucket_list_bloc.dart';
+
+abstract class BucketListState extends Equatable {
+  BucketListState({required this.items, this.errorMessage});
+
+  List<BucketListItem> items;
+  final String? errorMessage;
+
+  @override
+  List<Object?> get props => [items, errorMessage];
+}
+
+class BucketListInitialState extends BucketListState {
+  BucketListInitialState({required super.items});
+}
+
+class BucketListLoadingState extends BucketListState {
+  BucketListLoadingState() : super(items:  []);
+}
+
+class BucketListUpdatedState extends BucketListState {
+  BucketListUpdatedState({required super.items});
+}
+
+class BucketListErrorState extends BucketListState {
+  BucketListErrorState({required String message})
+      : super(items:  [], errorMessage: message);
+}
