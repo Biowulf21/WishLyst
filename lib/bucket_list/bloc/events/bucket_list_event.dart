2f733fdddef38bbceda72fe678040513e9971139
James Jilhaney <notail2111@gmail.com>
2023-09-14 11:23:34 +0800

    Refactor wish_list_event to bucket_list_event

---
+++ lib/bucket_list/bloc/events/bucket_list_event.dart
@@ -0,0 +1,11 @@
+sealed class BucketListItemEvent {}
+
+final class CreateBucketListItemEvent extends BucketListItemEvent {}
+
+final class GetBucketListItemsEvent extends BucketListItemEvent {}
+
+final class GetSingleBucketListItemEvent extends BucketListItemEvent {}
+
+final class UpdateBucketListItemEvent extends BucketListItemEvent {}
+
+final class DeleteBucketListItemEvent extends BucketListItemEvent {}
--- lib/bucket_list/bloc/events/wish_list_event.dart
+++
@@ -1,9 +0,0 @@
-sealed class WishListEvent {}
-
-final class WishListCreateEvent extends WishListEvent {}
-
-final class WishListGetEvent extends WishListEvent {}
-
-final class WishListUpdateEvent extends WishListEvent {}
-
-final class WishListDeleteEvent extends WishListEvent {}
