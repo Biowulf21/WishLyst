0f70e3da5d7c8f94ecbe9dcdfdadf41fa31e9968
James Jilhaney <notail2111@gmail.com>
2023-09-14 12:20:34 +0800

    Create: singleton class for the local database

---
+++ lib/app/database/local_db_singleton.dart
@@ -0,0 +1,53 @@
+import 'dart:async';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+import 'utils/local_db_config.dart';
+
+class LocalDatabaseSingleton {
+  LocalDatabaseSingleton._internal();
+
+  static final LocalDatabaseSingleton _singleton =
+      LocalDatabaseSingleton._internal();
+
+  late LocalDBConfig _config;
+  late Database _database;
+
+  factory LocalDatabaseSingleton(LocalDBConfig config) {
+    _singleton._config = config;
+    _singleton._initDB();
+    return _singleton;
+  }
+
+  Future<Database> get database async {
+    if (_database != null) {
+      return database;
+    }
+
+    _database = await _initDB();
+    return database;
+  }
+
+  Future<Database> _initDB() async {
+    final String databasesPath = await getDatabasesPath();
+    final String path = join(_config.path, _config.dbName);
+    return await openDatabase(path, version: 1, onCreate: _onCreate);
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    _database = await _initDB();
+
+    await _database.execute('''
+       CREATE TABLE IF NOT EXISTS bucket_list_items(
+         id INTEGER PRIMARY KEY AUTOINCREMENT,
+         itemName NOT NULL TEXT,
+         description TEXT,
+         isCompleted NOT NULL INTEGER,
+         dateCreated NOT NULL TEXT,
+         dateDeleted TEXT,
+         dateCompleted TEXT,
+         dateUpdated TEXT
+       )
+      ''');
+  }
+}
