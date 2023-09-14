6ae82ff2bf123217888cc659f8e43966f07f098b
James Jilhaney <notail2111@gmail.com>
2023-09-14 10:04:44 +0800

    Create: WishListBloc

---
+++ lib/bucket_list/bloc/wish_list_bloc/wish_list_bloc.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wishlyst/bucket_list/bloc/events/wish_list_event.dart';
+import 'package:wishlyst/bucket_list/model/wish_list_item.dart';
+
+class WishListBloc extends Bloc<WishListEvent, WishListItem> {
+
+  }
