b0b412486d9df8ea5d883d9d39b393dc134303d3
James Jilhaney <notail2111@gmail.com>
2023-09-14 17:03:10 +0800

    Refactor: BucketListEvent class

---
+++ lib/bucket_list/bucket_list_bloc/bucket_list_event.dart
@@ -0,0 +1,37 @@
+part of 'bucket_list_bloc.dart';
+
+@immutable
+sealed class BucketListEvent extends Equatable {
+  const BucketListEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class BucketListInitialEvent extends BucketListEvent {}
+
+class AddBucketListItemEvent extends BucketListEvent {
+  const AddBucketListItemEvent(this.bucketListItem);
+  final BucketListItem bucketListItem;
+
+  @override
+  List<Object?> get props => [bucketListItem];
+}
+
+class DeleteBucketListItemEvent extends BucketListEvent {
+  const DeleteBucketListItemEvent(this.id);
+  final int id;
+}
+
+class UpdateBucketListItemEvent extends BucketListEvent {
+  const UpdateBucketListItemEvent(this.bucketListItems);
+  final BucketListItem bucketListItems;
+
+  @override
+  List<Object?> get props => [bucketListItems];
+}
+
+class SetCompletedBucketListItemEvent extends BucketListEvent {
+  const SetCompletedBucketListItemEvent(this.id);
+  final int id;
+}
