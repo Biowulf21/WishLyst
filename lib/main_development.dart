921b1e9d0b8d4cc18e27ae02ce0bd787c71bd5bb
James Jilhaney <notail2111@gmail.com>
2023-09-14 12:21:45 +0800

    Create: different db instance per flavor

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,13 @@
+import 'package:flutter/material.dart';
 import 'package:wishlyst/app/app.dart';
+import 'package:wishlyst/app/database/local_db_singleton.dart';
+import 'package:wishlyst/app/database/utils/local_db_config.dart';
 import 'package:wishlyst/bootstrap.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  final config = LocalDBConfig(path: 'local/', dbName: 'dev_wishlyst.db');
+  final singleton = LocalDatabaseSingleton(config);
+
   bootstrap(() => const App());
 }
