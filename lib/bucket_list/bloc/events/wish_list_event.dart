aecb8b174ee992460a53aedcb35ce8b98eebdf24
James Jilhaney <notail2111@gmail.com>
2023-09-14 10:04:11 +0800

    Create: wishlist events

---
+++ lib/bucket_list/bloc/events/wish_list_event.dart
@@ -0,0 +1,9 @@
+sealed class WishListEvent {}
+
+final class WishListCreateEvent extends WishListEvent {}
+
+final class WishListGetEvent extends WishListEvent {}
+
+final class WishListUpdateEvent extends WishListEvent {}
+
+final class WishListDeleteEvent extends WishListEvent {}
