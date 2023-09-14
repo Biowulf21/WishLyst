09b767cc901f8929b1f15b5371f7eb52693b187c
James Jilhaney <notail2111@gmail.com>
2023-09-14 11:25:17 +0800

    Create: bucket_list.dart file for exports

---
+++ lib/bucket_list/bucket_list.dart
@@ -0,0 +1,5 @@
+export './bloc/events/bucket_list_event.dart';
+export './bloc/bucket_list_bloc/bucket_list_bloc.dart';
+export './data/model/bucket_list_item.dart';
+export './data/repository/bucket_list_repository.dart';
+export './view/bucket_list_page.dart';
