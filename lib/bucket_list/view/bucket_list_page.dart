2da9059681043550790a1e9d510a4efb7074008a
James Jilhaney <notail2111@gmail.com>
2023-09-15 12:20:19 +0800

    Add: bottom modal for creating new bucket list item

--- lib/bucket_list/view/bucket_list_page.dart
+++ lib/bucket_list/view/bucket_list_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:wishlyst/bucket_list/bucket_list.dart';
 import 'package:wishlyst/bucket_list/bucket_list_bloc/bucket_list_bloc.dart';
-import 'package:wishlyst/bucket_list/data/model/bucket_list_item.dart';
 import 'package:wishlyst/bucket_list/data/repository/local_bucket_list_repository.dart';
 import 'package:wishlyst/bucket_list/view/widgets/bucket_list_item_widget.dart';
 
@@ -46,7 +44,6 @@ class BucketListView extends StatelessWidget {
             return ListView.builder(
               itemCount: state.items.length,
               itemBuilder: (context, index) {
-                print('item count is ${state.items.length}');
                 final item = state.items[index];
                 return const BucketListItemWidget();
               },
@@ -59,16 +56,48 @@ class BucketListView extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () {
-          final bucketListItem = BucketListItem(
-            itemName: 'test',
-            description: 'desc',
-            dateCreated: DateTime.now(),
-          );
-
-          bloc.add(AddBucketListItemEvent(bucketListItem));
-        },
+        onPressed: () => _showNewBucketListItemModal(context),
       ),
     );
   }
+}
+
+void _showNewBucketListItemModal(BuildContext context) {
+  showModalBottomSheet(
+    context: context,
+    builder: (BuildContext context) {
+      return Container(
+        height: MediaQuery.of(context).size.height / 2,
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const Text(
+              'Add New Bucket List Item',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Item Name'),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Description'),
+              maxLines: 3,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Add Item'),
+            ),
+          ],
+        ),
+      );
+    },
+  );
 }
