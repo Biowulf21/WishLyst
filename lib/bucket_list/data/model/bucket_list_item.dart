c5f69f486b05e5573417d9652e16e026e043910f
James Jilhaney <notail2111@gmail.com>
2023-09-15 12:01:12 +0800

    Update: bucketlist item model

--- lib/bucket_list/data/model/bucket_list_item.dart
+++ lib/bucket_list/data/model/bucket_list_item.dart
@@ -1,44 +1,21 @@
 import 'package:equatable/equatable.dart';
 
 class BucketListItem extends Equatable {
-  int id;
-  String itemName;
-  bool? isComplete = false;
-  String description;
-  DateTime dateCreated;
-  DateTime? dateDeleted;
-  DateTime? dateCompleted;
-  DateTime? dateUpdated;
-
   BucketListItem(
-      {required this.id,
-      required this.itemName,
+      {required this.itemName,
       required this.description,
       required this.dateCreated,
-      this.isComplete,
+      this.isComplete = false,
       this.dateDeleted,
       this.dateCompleted,
       this.dateUpdated});
 
-  @override
-  List<Object?> get props => [
-        id,
-        itemName,
-        description,
-        dateCreated,
-        isComplete,
-        dateDeleted,
-        dateCompleted,
-        dateUpdated
-      ];
-
   factory BucketListItem.fromJson(Map<String, dynamic> json) {
     return BucketListItem(
-      id: json['id'] as int,
       itemName: json['itemName'] as String,
       description: json['description'] as String,
       dateCreated: DateTime.parse(json['dateCreated'] as String),
-      isComplete: json['isComplete'] as bool,
+      isComplete: json['isComplete'] == 1 ? true : false,
       dateDeleted: json['dateDeleted'] != null
           ? DateTime.parse(json['dateDeleted'] as String)
           : null,
@@ -50,9 +27,26 @@ class BucketListItem extends Equatable {
           : null,
     );
   }
+  String itemName;
+  bool? isComplete;
+  String description;
+  DateTime dateCreated;
+  DateTime? dateDeleted;
+  DateTime? dateCompleted;
+  DateTime? dateUpdated;
+
+  @override
+  List<Object?> get props => [
+        itemName,
+        description,
+        dateCreated,
+        isComplete,
+        dateDeleted,
+        dateCompleted,
+        dateUpdated
+      ];
 
   Map<String, dynamic> toJson() => {
-        'id': id,
         'itemName': itemName,
         'description': description,
         'dateCreated': dateCreated.toIso8601String(),
@@ -64,7 +58,6 @@ class BucketListItem extends Equatable {
 
   BucketListItem copyWith() {
     return BucketListItem(
-      id: id,
       itemName: itemName,
       description: description,
       dateCreated: dateCreated,
